78b0b715b73272196044e40fa4bf741fff33a521
Naveen Raj Y <naveenrajy@yahoo.com>
2020-11-23 13:48:06 +0530

    added primitive types

---
+++ main3.ts
@@ -0,0 +1,19 @@
+// let x: any;
+let x;
+x = 10;
+x = true;
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [10, 20, 30, 40];
+let f: any[] = [10, "hello", true, 'a'];
+
+const ColorRed = '#FF0000';
+const ColorGreen = '#00FF00';
+const ColorBlue = '#0000FF';
+
+enum Color {RED=0, GREEN=1, BLUE=2};
+
+let backgroundColor = Color.GREEN;
